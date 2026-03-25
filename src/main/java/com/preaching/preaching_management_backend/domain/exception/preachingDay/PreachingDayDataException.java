7fe17634a563e5e5d6b4a482abf4f5a04edeb51b
JoseRiv4s <navedasjose@gmail.com>
2026-03-25 14:33:23 -0500

    feat: agregar PreachingDayService y renombrar la excepción de datos para mayor claridad

--- src/main/java/com/preaching/preaching_management_backend/domain/exception/preachingDay/PreachingDayDataException.java
+++
@@ -1,7 +0,0 @@
-package com.preaching.preaching_management_backend.domain.exception.preachingDay;
-
-public class PreachingDayDataException extends RuntimeException {
-    public PreachingDayDataException(String message) {
-        super(message);
-    }
-}
